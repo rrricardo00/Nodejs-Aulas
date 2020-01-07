ebfc02473922ec89a945ee94eca8b40767a01b8f
rrricardo00 <rrricardo00@yahoo.com.br>
2020-01-07 00:58:58 -0300

    alpha

---
+++ app.js
@@ -0,0 +1,5 @@
+let somaFunction = require('./calc');
+
+
+const mult = somaFunction.mult(10, 2);
+console.log(mult);
---
+++ calc.js
@@ -0,0 +1,12 @@
+function soma(a, b){
+     return a+b
+}
+function mult(a, b){
+    return a*b;
+}
+
+
+
+module.exports = {
+    soma, mult
+};
