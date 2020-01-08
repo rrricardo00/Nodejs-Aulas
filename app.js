d2db1bb4e6e9044337327df1fa1c074d47c9e133
rrricardo00 <rrricardo00@yahoo.com.br>
2020-01-07 23:54:52 -0300

    Alpha

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ consign().include('rotas').into(app);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/ola/:nome', (req, res) => {
-    let nome = req.params.nome
-    res.send('Olá, ' + nome);
+app.get('/blog/:artigo?', (req, res) => {
+    let artigo = req.params.artigo;
+    if (artigo)
+        res.send(artigo)
+    else
+        res.send('não passou o parametro artigo')
+
 });
 
-app.get('/ola/:nome/:empresa', (req, res) => {
+app.get('/ola/:nome?/:empresa?', (req, res) => {
     let nome = req.params.nome
     res.send('Olá, ' + nome);
 });
